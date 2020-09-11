ecfa55aa38e6007121c5fcd6b6d8d134be226da7
Vinicius Benedito <beneditoloura@gmail.com>
2020-09-11 08:07:43 -0300

    API Wordpress

---
+++ mobile/APITest/App.tsx
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from 'react';
+import { View, Text, TouchableHighlight, StyleSheet, ScrollView } from 'react-native';
+
+import axios from 'axios';
+
+interface Item {
+  id: string;
+  title: string;
+  link: string;
+  categories: string;
+}
+
+const App = () => {
+
+  const [items, setItems] = useState<Item[]>([]);
+
+  useEffect(() => {
+    axios.get<Item[]>('http://lemonflex.com.br/wp-json/wp/v2/posts')
+      .then(response => {
+        setItems(response.data);
+      });
+  }, []);
+
+  return (
+    <ScrollView>
+      {items.map((item) => (
+        <View key={item.id}>
+          <Text>{item.categories}</Text>
+          <Text>{JSON.stringify(item.title).replace('{"rendered":"', '').replace('"}', '')}</Text>
+          <TouchableHighlight style={styles.btn}>
+            <Text>{item.link}</Text>
+          </TouchableHighlight>
+        </View>
+      ))}
+    </ScrollView>
+  );
+};
+
+
+const styles = StyleSheet.create({
+  btn: {
+    backgroundColor: '#0f0',
+    padding: 10,
+    marginBottom: 20
+
+  }
+})
+
+export default App;
